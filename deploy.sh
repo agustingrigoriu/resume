a8f73fc8aa3b7f7eda74af5e5575ba366c18ce21
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2018-05-17 23:00:04 -0300

    Modificaciones varias

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
 scp -r  ./build/ grigo:/home/agustin/Development/resume/
+scp -r  ./public/ grigo:/home/agustin/Development/resume/
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,74 +3,85 @@ import React, { Component } from 'react';
 
 class Resume extends Component {
   render() {
-    if(this.props.data){
-      var education = this.props.data.education.map(function(edu){
+    if (this.props.data) {
+      var education = this.props.data.education.map(function (edu) {
+        var historic_average = edu.historic_average && (
+          <span>
+            &bull;
+            <em className="date"> Historic Average: <b>{edu.historic_average}</b></em>
+          </span>) || '';
+        console.log(historic_average);
         return <div key={edu.school} className="row item">
-           <div className="twelve columns">
-              <h3>{edu.school}</h3>
-              <p className="info">{edu.degree} <span>&bull;</span> <em className="date">{edu.graduated}</em></p>
-              <p>
-                {edu.description}
-              </p>
-           </div>
+          <div className="twelve columns">
+            <h3>{edu.school}</h3>
+            <p className="info">
+              {edu.degree}
+              <span>&bull;</span>
+              <em className="date">{edu.graduated}</em>
+              {historic_average}
+            </p>
+            <p>
+              {edu.description}
+            </p>
+          </div>
         </div>
       });
 
-      var work = this.props.data.work.map(function(job){
+      var work = this.props.data.work.map(function (job) {
         return <div key={job.company} className="row item">
-           <div className="twelve columns">
-              <h3>{job.company}</h3>
-              <p className="info">{job.title}<span>&bull;</span> <em className="date">{job.years}</em></p>
+          <div className="twelve columns">
+            <h3>{job.company}</h3>
+            <p className="info">{job.title}<span>&bull;</span> <em className="date">{job.years}</em></p>
 
-              <p>
+            <p>
               {job.description}
-              </p>
-           </div>
+            </p>
+          </div>
         </div>
       });
 
-      var skills = this.props.data.skills.map(function(skill){
-        var className = 'bar-expand '+skill.name.toLowerCase();
-        return <li key={skill.name}><span style={{width:skill.level}} className={className}></span><em>{skill.name}</em></li>
+      var skills = this.props.data.skills.map(function (skill) {
+        var className = 'bar-expand ' + skill.name.toLowerCase();
+        return <li key={skill.name}><span style={{ width: skill.level }} className={className}></span><em>{skill.name}</em></li>
       });
     }
     return (
       <section id="resume">
-      <div className="row education">
-         <div className="three columns header-col">
+        <div className="row education">
+          <div className="three columns header-col">
             <h1><span>Education</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             {education}
-         </div>
-      </div>
+          </div>
+        </div>
 
-      <div className="row work">
-         <div className="three columns header-col">
+        <div className="row work">
+          <div className="three columns header-col">
             <h1><span>Work</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
-          {work}
-         </div>
-      </div>
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
 
-      <div className="row skill">
-         <div className="three columns header-col">
+        <div className="row skill">
+          <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
-         <div className="nine columns main-col">
+          </div>
+          <div className="nine columns main-col">
             <p>The main skill sets below outline the variety of skills performed within my roles at my current jobs and experience given by my formation as an engineer.
             </p>
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div className="bars">
+              <ul className="skills">
+                {skills}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
