e1a91c917e666fe063f07ab79fab336987839a90
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2018-05-19 12:02:35 -0300

    Modifico archivo de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-scp -r  ./build/ grigo:/home/agustin/Development/resume/
-scp -r  ./public/ grigo:/home/agustin/Development/resume/
+#!/bin/bash
+echo "Borrando el tar anterior"
+rm build.tar.gz
+echo "Generando el tar nuevo"
+tar -czvf build.tar.gz ./build
+echo "Copiando el tar al servidor por scp"
+scp build.tar.gz grigo:/home/agustin/Development/resume
+echo "Descomprimiendo el sitio y borrando el tar por ssh"
+ssh grigo 'cd ~/Development/resume ; tar -xvf build.tar.gz -C build ; rm build.tar.gz'
