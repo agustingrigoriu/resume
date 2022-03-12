04188ab95c4abe26757b6bb42faafca0b38f89d7
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2022-03-12 17:58:30 -0500

    Updates

---
+++ precache-manifest.b2a1a6edf9b6a0d04e779b83595022a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c1e3b1b8337ca0d0482",
+    "url": "/resume/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "3c1e3b1b8337ca0d0482",
+    "url": "/resume/static/js/main.dae711b8.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "5874e059d2d9457b44b8",
+    "url": "/resume/static/css/2.922eae36.chunk.css"
+  },
+  {
+    "revision": "5874e059d2d9457b44b8",
+    "url": "/resume/static/js/2.3b10014a.chunk.js"
+  },
+  {
+    "revision": "b7cc8405f0abbc0a498aab876d330f88",
+    "url": "/resume/index.html"
+  }
+];
